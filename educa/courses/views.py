6c03f672119e961a3396a5b3d29b1043280a79e2
mwong-wtamu <swong1@buffs.wtamu.edu>
2024-11-14 19:44:03 -0600

    Chapter 14 Cache Fix

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -166,7 +166,7 @@ class CourseListView(TemplateResponseMixin, View):
         subjects = cache.get("all_subjects")
         if not subjects:
             subjects = Subject.objects.annotate(total_courses=Count("courses"))
-        cache.set("all_subjects", subjects)
+            cache.set("all_subjects", subjects)
         all_courses = Course.objects.annotate(total_modules=Count("modules"))
         if subject:
             subject = get_object_or_404(Subject, slug=subject)
@@ -175,11 +175,11 @@ class CourseListView(TemplateResponseMixin, View):
             if not courses:
                 courses = all_courses.filter(subject=subject)
                 cache.set(key, courses)
-            else:
-                courses = cache.get("all_courses")
-                if not courses:
-                    courses = all_courses
-                    cache.set("all_courses", courses)
+        else:
+            courses = cache.get("all_courses")
+            if not courses:
+                courses = all_courses
+                cache.set("all_courses", courses)
         return self.render_to_response(
             {"subjects": subjects, "subject": subject, "courses": courses}
         )
